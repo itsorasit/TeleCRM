1bf1a6bfa7677787392681dc990a7152942641b4
itsorasit <itsorasit@gmail.com>
2024-09-06 08:11:19 +0700

    tun

---
+++ BlazorApp_TeleCRM/Program.cs
@@ -0,0 +1,40 @@
+using BlazorApp_TeleCRM.Components;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Radzen;
+using Radzen.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register Radzen services
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped<ThemeService>(); // Register ThemeService
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<Radzen.ContextMenuService>();
+builder.Services.AddScoped<Radzen.TooltipService>();
+builder.Services.AddScoped<Radzen.SideDialogOptions>();
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode(); // Ensure this matches your setup
+
+app.Run();
