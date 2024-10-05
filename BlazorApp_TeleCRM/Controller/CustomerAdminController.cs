aa2e66f3253efc2e39f01293066ab25cb8776650
itsorasit <sarayut.s@mcgroupnet.com>
2024-10-05 21:23:28 +0700

    tun

--- BlazorApp_TeleCRM/Controller/CustomerAdminController.cs
+++ BlazorApp_TeleCRM/Controller/CustomerAdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlazorApp_TeleCRM.Data;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace BlazorApp_TeleCRM.Controller
 {
@@ -6,6 +8,16 @@ namespace BlazorApp_TeleCRM.Controller
     [Route("api/[controller]")]
     public class CustomerAdminController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public CustomerAdminController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -89,6 +101,91 @@ namespace BlazorApp_TeleCRM.Controller
             public string Remark4 { get; set; }
             public string Remark5 { get; set; }
 
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> GetCustomerData([FromBody] SearchCriteria searchCriteria)
+        {
+
+            DateTime today = DateTime.Now;
+
+            var customers = new List<CustomerDataList>();
+
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+
+                var query = "";
+
+                if (searchCriteria.fdate == null || searchCriteria.ldate == null)
+                {
+                    query = @"SELECT guid, code, name, phone, address1, sub_district, district, province, zipcode, datasource_platform, social_media, branch_code, created_by, created_date, modified_by, modified_date
+                          FROM mas_customers
+                          WHERE branch_code='000000' order by modified_date desc LIMIT 500 ";
+
+                    searchCriteria.fdate = today;
+                    searchCriteria.ldate = today;
+                }
+                else {
+                    query = @"SELECT guid, code, name, phone, address1, sub_district, district, province, zipcode, datasource_platform, social_media, branch_code, created_by, created_date, modified_by, modified_date
+                          FROM mas_customers
+                          WHERE modified_date >= @FromDate 
+                          AND modified_date <= @ToDate 
+                          AND branch_code='000000' 
+                          order by modified_date desc ";
+
+                }
+
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    // กำหนดค่าช่วงเวลาของ FromDate เป็นเริ่มต้นของวัน
+                    var fromDate = searchCriteria.fdate?.Date ?? DateTime.MinValue.Date;
+                    cmd.Parameters.AddWithValue("@FromDate", fromDate);
+
+                    // กำหนดค่าช่วงเวลาของ ToDate เป็นสิ้นสุดของวัน
+                    var toDate = (searchCriteria.ldate?.Date ?? DateTime.MaxValue.Date).AddDays(1).AddTicks(-1);
+                    cmd.Parameters.AddWithValue("@ToDate", toDate);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var customer = new CustomerDataList
+                            {
+                                guid = reader.GetString(0),
+                                code = reader.GetString(1),
+                                name = reader.GetString(2),
+                                phone = reader.GetString(3),
+                                address1 = reader.GetString(4),
+                                sub_district = reader.GetString(5),
+                                district = reader.GetString(6),
+                                province = reader.GetString(7),
+                                zipcode = reader.GetString(8),
+                                datasource_platform = reader.GetString(9),
+                                social_media = reader.GetString(10),
+                                branch_code = reader.GetString(11),
+                                created_by = reader.GetString(12),
+                                created_date = reader.GetDateTime(13),
+                                modified_by = reader.IsDBNull(14) ? null : reader.GetString(14),
+                                modified_date = reader.IsDBNull(15) ? (DateTime?)null : reader.GetDateTime(15)
+                            };
+
+                            customers.Add(customer);
+                        }
+                    }
+                }
+            }
+
+            return Ok(customers);
+        }
+
+
+        public class SearchCriteria
+        {
+            public DateTime? fdate { get; set; }
+            public DateTime? ldate { get; set; }
         }
     }
 }
---
+++ BlazorApp_TeleCRM/Data/CustomerDataList.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorApp_TeleCRM.Data
+{
+    public class CustomerDataList
+    {
+        public string guid { get; set; }
+        public string? code { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string? address1 { get; set; }
+        public string? sub_district { get; set; }
+        public string? district { get; set; }
+        public string? province { get; set; }
+        public string? zipcode { get; set; }
+        public string? datasource_platform { get; set; }
+        public string? social_media { get; set; }
+        public string branch_code { get; set; }
+        public string created_by { get; set; }
+        public DateTime? created_date { get; set; }
+        public string? modified_by { get; set; }
+        public DateTime? modified_date { get; set; }
+        public int?  count_activity { get; set; }
+        public DateTime? activity_lastdate { get; set; }
+
+    }
+}
