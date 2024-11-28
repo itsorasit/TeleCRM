82b305b2cf3eee5020fe5d072b685ac9b89ed6ef
itsorasit <sarayut.s@mcgroupnet.com>
2024-11-28 17:46:49 +0700

    tun

--- BlazorApp_TeleCRM/Models/User.cs
+++ BlazorApp_TeleCRM/Models/User.cs
@@ -2,14 +2,14 @@
 {
     public class User
     {
-        public int id { get; set; }
-        public string username { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string firstname { get; set; }
-        public string lastname { get; set; }
-        public string role { get; set; }
-        public string organization { get; set; }
+        public int? id { get; set; }
+        public string? username { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+        public string? firstname { get; set; }
+        public string? lastname { get; set; }
+        public string? role { get; set; }
+        public string? organization { get; set; }
         public string? loginkey { get; set; }
         public DateTime? lastlogin { get; set; }
     }
